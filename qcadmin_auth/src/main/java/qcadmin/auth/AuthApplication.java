ea0b2f3784a9ae56d38fac5e90670af17c9d6881
niemiao <905336559@qq.com>
2019-03-27 19:27:46 +0800

    2019年3月27日：公钥提交

---
+++ qcadmin_auth/src/main/java/qcadmin/auth/AuthApplication.java
@@ -0,0 +1,17 @@
+package qcadmin.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: springboot-qcadmin
+ * @description: 启动类
+ * @author: NieMiao
+ * @create: 2019-03-27 14:55
+ **/
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class,args);
+    }
+}
